084c8ad4ac5ad6189f6324c5c0609e676dfdcf96
sahan <hyanjusahan@gmail.com>
2023-09-15 12:29:06 +0545

    profile update delete get

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Profile = require("../model/profileModel");
+const profileModel = require("../model/profileModel");
 const domain = "http://localhost:3000";
 const auth = require("../config/auth.js");
 const uploadServices = require("../services/uploadServices");
@@ -8,8 +8,11 @@ const uploadServices = require("../services/uploadServices");
 // @route POST profile/create by taking the ref of the user
 // @desc Create a profile
 // @access Private
-router.post("/profile/create",  uploadServices.profileImage.single("profileimage"),
-  auth.verifyUser,  async (req, res) => {
+router.post(
+  "/profile/create",
+  uploadServices.profileImage.single("profileimage"),
+  auth.verifyUser,
+  async (req, res) => {
     const data = req.body;
     const file = req.file;
     try {
@@ -17,7 +20,7 @@ router.post("/profile/create",  uploadServices.profileImage.single("profileimage
         return res.status(400).send("Please upload an image");
       }
       const image = domain + "/public/profiles/" + file.filename;
-      const profile = new Profile({
+      const profile = new profileModel({
         user: req.userData._id, // req.userData in auth js ma banako xa
         name: data.name,
         contact: data.address,
@@ -33,5 +36,77 @@ router.post("/profile/create",  uploadServices.profileImage.single("profileimage
     }
   }
 );
+// @route put profile/update by taking the ref of the user
+// @desc update a profile
+// @access Private
+router.put(
+  "/profile/update",
+  uploadServices.profileImage.single("profileimage"),
+  auth.verifyUser,
+  async (req, res) => {
+    const data = req.body;
+    const file = req.file;
+    try {
+      const profile = await profileModel.findOne({ user: req.userData._id });
+      if (!profile) {
+        res.status(400).json({ msg: "profile not found" });
+        return;
+      }
+      if (!file || file.length === 0) {
+        profile.name = data.name ? data.name : profile.name;
+        profile.contact = data.contact ? data.contact : profile.contact;
+        profile.address = data.address ? data.address : profile.address;
+        profile.profilepic = profile.profilepic;
+        const updatedProfile = await profile.save();
+        res.json({ msg: "profile updated", success: true, updatedProfile });
+      } else {
+        const image = domain + "/public/profiles" + file.filename;
+        profile.name = data.name ? data.name : profile.name;
+        profile.profileimage = image ? image : profile.profileimage;
+        profile.contact = data.contact ? data.contact : profile.contact;
+        profile.address = data.address ? data.address : profile.address;
+        profile.dob = data.dob ? data.dob : profile.dob;
 
+        const updatedProfile = await profile.save();
+        res.json({ msg: "Profile updated", success: true, updatedProfile });
+      }
+    } catch (e) {
+      res.status(500).json({ msg: e.message, success: false });
+    }
+  }
+);
+// code for get the profile by taking the ref of the user
+// @route GET profile/get
+// @desc Get a profile
+// @access Private
+router.get("/profile/get", auth.verifyUser, async (req, res) => {
+  try {
+    const profile = await profileModel.findOne({ user: req.userData._id });
+    if (!profile) {
+      return res.status(400).send("Profile not found");
+    }
+    res.json({ msg: "profile fetched", success: true, profile });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Server Error");
+  }
+});
+
+// code for delete the profile by taking the ref of the user
+// @route DELETE profile/delete
+// @desc Delete a profile
+// @access Private
+router.delete("/profile/delete", auth.verifyUser, async (req, res) => {
+  try {
+    const profile = await profileModel.findOne({ user: req.userData._id });
+    if (!profile) {
+      return res.status(400).send("Profile not found");
+    }
+    await profile.deleteOne;
+    res.json({ msg: "profile deleted", success: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
